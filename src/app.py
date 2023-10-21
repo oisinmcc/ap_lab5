2621ccac57fdc9634bf51415cb1bd78aab281c13
vakaloud <alex.vakaloudis@cit.ie>
2023-10-21 08:47:43 +0100

    removed workflows

--- src/app.py
+++ src/app.py
@@ -3,6 +3,10 @@ def squares(x):
     y = x ** 2
     return y
 
+def cube(x):
+    # Perform calculus
+    y = x ** 3
+    return y
 
 if __name__ == "__main__":
     print(f"The square of 20 is: {squares(20)}")
